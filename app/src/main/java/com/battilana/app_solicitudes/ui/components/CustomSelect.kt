201b70a4b0bcf20f0156b8e0636e112b029d15e1
Fredy <inztockz@gmail.com>
2025-11-15 11:46:17 -0500

    Editando el customSelect para que sea un buscador manual en vez de lista completa

--- app/src/main/java/com/battilana/app_solicitudes/ui/components/CustomSelect.kt
+++ app/src/main/java/com/battilana/app_solicitudes/ui/components/CustomSelect.kt
@@ -1,10 +1,13 @@
 package com.battilana.app_solicitudes.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.DropdownMenu
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
@@ -19,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlin.math.exp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,39 +31,80 @@ fun <T> BattiSelect(
     options: List<T>,
     selectedOption: T?,
     labelMapper: (T) -> String,
-    onSelected: (T) -> Unit
+    onSelected: (T) -> Unit,
+    singleLine: Boolean = true
 ) {
     var expanded by remember { mutableStateOf(false) }
+    var searchQuery by remember { mutableStateOf("") }
+
+    val filteredOptions = remember(searchQuery, options) {
+        if (searchQuery.isEmpty()) options
+        else options.filter {
+            labelMapper(it).contains(searchQuery, ignoreCase = true)
+        }
+    }
 
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = { expanded = !expanded }
     ) {
         TextField(
-            value = selectedOption?.let(labelMapper) ?: "",
-            onValueChange = {},
-            readOnly = true,
+            value = searchQuery.ifEmpty { selectedOption?.let(labelMapper) ?: "" },
+            onValueChange = {
+                searchQuery = it
+                expanded = true
+            },
             label = { Text(text = label) },
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded) },
             modifier = Modifier
                 .menuAnchor(type = ExposedDropdownMenuAnchorType.PrimaryNotEditable, enabled = true)
                 .fillMaxWidth(),
-            shape = RoundedCornerShape(10.dp)
+            shape = RoundedCornerShape(10.dp),
+            singleLine = singleLine
         )
         ExposedDropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
             modifier = Modifier.heightIn(max = 300.dp)
         ) {
-            options.forEach { option ->
+
+            if (filteredOptions.isEmpty()) {
                 DropdownMenuItem(
-                    text = { Text(text = labelMapper(option)) },
-                    onClick = {
-                        onSelected(option)
-                        expanded = false
-                    }
+                    text = { Text(text = "Sin resultados") },
+                    onClick = {}
                 )
+            } else {
+                filteredOptions.forEach { option ->
+                    DropdownMenuItem(
+                        text = {Text(text = labelMapper(option))},
+                        onClick = {
+                            onSelected(option)
+                            searchQuery = labelMapper(option)
+                            expanded = false
+                        }
+                    )
+                }
             }
+
+//            filteredOptions.forEach { option ->
+//                DropdownMenuItem(
+//                    text = {Text(text = labelMapper(option))},
+//                    onClick = {
+//                        onSelected(option)
+//                        searchQuery = labelMapper(option)
+//                        expanded = false
+//                    }
+//                )
+//            }
+//            options.forEach { option ->
+//                DropdownMenuItem(
+//                    text = { Text(text = labelMapper(option)) },
+//                    onClick = {
+//                        onSelected(option)
+//                        expanded = false
+//                    }
+//                )
+//            }
         }
     }
 }
--- app/src/main/java/com/battilana/app_solicitudes/ui/screens/historialpedidos/HistorialPedidosViewModel.kt
+++ app/src/main/java/com/battilana/app_solicitudes/ui/screens/historialpedidos/HistorialPedidosViewModel.kt
@@ -1,6 +1,11 @@
 package com.battilana.app_solicitudes.ui.screens.historialpedidos
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
 
-class HistorialPedidosViewModel: ViewModel() {
+@HiltViewModel
+class HistorialPedidosViewModel @Inject constructor(): ViewModel() {
+
+
 }
